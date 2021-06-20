84a8073c1992f2ff65ed3cdc04bb0b302b2f8274
Nandagopal <ethuku1027@gmail.com>
2021-06-20 06:52:31 +0530

    REFACTOR CODE

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,11 @@
-const loading = document.querySelector('#loading');
-const api_key = '18e911668c4244151b14e89d54d25c29';
-
-// main container:
+const loadingAnimation = document.querySelector('#loading');
 const page1Container = document.querySelector('#page1Container');
 const page2Container = document.querySelector('#page2Container');
-
-// inside of main container:
 const showHoldingContainer = document.querySelector('#showHoldingContainer');
 const animeHoldingContainer = document.querySelector('#animeHoldingContainer');
+const api_key = '18e911668c4244151b14e89d54d25c29';
 
 
-
-// remove existing childeren from the given parent:
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
@@ -19,9 +13,8 @@ function removeAllChildNodes(parent) {
 }
 
 
-// return response of the given fetch urls:
 async function fetchFunction(url){
-    loading.style.display = 'visible';
+    loadingAnimation.style.display = 'visible';
     return await fetch(url)
         .then(response => response.json())
         .then(response => {
@@ -31,42 +24,37 @@ async function fetchFunction(url){
 }
 
 
-// 1.remove all children and create showContainer,
-// 2.append showcontainer to popularShowContainer or popularAnimeContainer,
-// 3.append popular..container to showHoldingcontainer or animeHoldingContainer,
-// 4.append holding..container to page1Container,
-// 5.when user clicks name of show -> redirect to next page, save name to localstorgae.
-function displayShows(shows,container){
-    removeAllChildNodes(container);    
-    shows.forEach(show => {
+function renderShowsandAnime(results,location){
+    removeAllChildNodes(location);    
+    results.forEach(result => {
         const showContainer = document.createElement('div');
-        let imageSource = show.poster_path;
-        if(show.poster_path === '' || show.poster_path === null) return; //edge cases:
+        let imageSource = result.poster_path;
+        if(result.poster_path === '' || result.poster_path === null) return; //if image not found:
         showContainer.classList.add('show-container');
         showContainer.innerHTML = `
-            <img class="show-img" loading="lazy" src="https://image.tmdb.org/t/p/w500${imageSource}" alt="show image">
+            <img class="show-img" src="https://image.tmdb.org/t/p/w500${imageSource}" alt="show image">
             <div class="show-contents">
-                <h2 class="show-title"  id="${show.id}">${show.name}</h2>
-                <h2 class="show-rating">${show.vote_average}</h2>
+                <h2 class="show-title"  id="${result.id}">${result.name}</h2>
+                <h2 class="show-rating">${result.vote_average}</h2>
             </div>
         `;
-        if(container.className === 'search-results-container'){
+        if(location.className === 'search-results-container'){
             removeAllChildNodes(page1Container);
-            container.appendChild(showContainer);
-            page1Container.appendChild(container);
+            location.appendChild(showContainer);
+            page1Container.appendChild(location);
         }
-        if(container.className === 'popular-show-container'){
+        if(location.className === 'popular-show-container'){
             removeAllChildNodes(showHoldingContainer);
-            container.appendChild(showContainer);
-            showHoldingContainer.appendChild(container)
+            location.appendChild(showContainer);
+            showHoldingContainer.appendChild(location)
         }
-        if(container.className === 'popular-anime-container'){
+        if(location.className === 'popular-anime-container'){
             removeAllChildNodes(animeHoldingContainer);
-            container.appendChild(showContainer);
-            animeHoldingContainer.appendChild(container);
+            location.appendChild(showContainer);
+            animeHoldingContainer.appendChild(location);
         }
 
-        // when user clicks particular show:
+        // save show id to local storage:
         showContainer.addEventListener('click',(e) => {
             if(e.target.className === 'show-title'){
                 let ID = e.target.id;
@@ -78,29 +66,26 @@ function displayShows(shows,container){
 }
 
 
-// 1.create new container for the selected show,
-// 2.append selected show to the new container,
-// 2.append new container to page2container.
-function displayParticularShow(show){
+function renderParticularShow(result){
     const showTitle = document.createElement('h1');
     showTitle.classList.add('specific-show-name')
-    showTitle.textContent = show.name;
+    showTitle.textContent = result.name;
 
     const overviewContainer = document.createElement('div');
     overviewContainer.classList.add('overview');
 
-    let imageSource = show.poster_path;
+    let imageSource = result.poster_path;
     overviewContainer.innerHTML = `
-        <img class="overview-image" loading="lazy" src="https://image.tmdb.org/t/p/w500${imageSource}" alt="show-image">
-        <p class="overview-summary">${show.overview}</p>
+        <img class="overview-image" src="https://image.tmdb.org/t/p/w500${imageSource}" alt="show-image">
+        <p class="overview-summary">${result.overview}</p>
         <div class="show-details-container">
-            <h3 class="show-details"><strong>Premiered:  </strong>${show.first_air_date}</h3>
-            <h3 class="show-details"><strong>Popularity:  </strong>${show.popularity}</h3>
-            <h3 class="show-details"><strong>Vote Average:  </strong>${show.vote_average}</h3>
-            <h3 class="show-details"><strong>Vote Count:  </strong>${show.vote_count}</h3>
-            <h3 class="show-details"><strong>No of seasons:  </strong>${show.number_of_seasons}</h3>
-            <h3 class="show-details"><strong>No of episodes:  </strong>${show.number_of_episodes}</h3>
-            <h3 class="show-details"><strong>link:  </strong><a href="${show.homepage}" target="_blank">${show.homepage}</a></h3>
+            <h3 class="show-details"><strong>Premiered:  </strong>${result.first_air_date}</h3>
+            <h3 class="show-details"><strong>Popularity:  </strong>${result.popularity}</h3>
+            <h3 class="show-details"><strong>Vote Average:  </strong>${result.vote_average}</h3>
+            <h3 class="show-details"><strong>Vote Count:  </strong>${result.vote_count}</h3>
+            <h3 class="show-details"><strong>No of seasons:  </strong>${result.number_of_seasons}</h3>
+            <h3 class="show-details"><strong>No of episodes:  </strong>${result.number_of_episodes}</h3>
+            <h3 class="show-details"><strong>link:  </strong><a href="${result.homepage}" target="_blank">${result.homepage}</a></h3>
         </div>
     `;
     page2Container.appendChild(showTitle);
@@ -108,69 +93,71 @@ function displayParticularShow(show){
 }
 
 
-// 1.get popular 20 shows from the given fetch url,
-// 2.call displayShows function for display it.
-function getPopularShows(){
 
-    // get 20shows:
+function getPopularShows(){
     const showUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&sort_by=popularity.desc&append_to_response=images&include_image_language=en,null`;
     let popularShowContainer = document.createElement('div');
     popularShowContainer.classList.add('popular-show-container');
     let popularShows = fetchFunction(showUrl);
     popularShows.then(response => {
-        loading.style.display = 'none';
-        displayShows(response.results,popularShowContainer);
+        loadingAnimation.style.display = 'none';
+        renderShowsandAnime(response.results,popularShowContainer);
     });
+};
 
-    
-    // get 20animes:
+function getPopularAnime(){
     const animeUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&sort_by=popularity.desc&with_keywords=210024`;
     let popularAnimeContainer = document.createElement('div');
     popularAnimeContainer.classList.add('popular-anime-container');
     let popularAnimes = fetchFunction(animeUrl);
     popularAnimes.then(response => {
-        loading.style.display = 'none';
-        displayShows(response.results,popularAnimeContainer);
+        loadingAnimation.style.display = 'none';
+        renderShowsandAnime(response.results,popularAnimeContainer);
     })
 };
+
+
+function searchShowsandAnime(){
+    const input = document.querySelector('#input');
+    const searchBtn = document.querySelector('#btn');
+    searchBtn.addEventListener('click',()=>{
+        if(input.value === '' || input.value === null) {
+            window.location.replace('index.html');
+        };
+        const inputValue = input.value;
+        const url = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${inputValue}`;
+        let searchResultsContainer = document.createElement('div');
+        searchResultsContainer.classList.add('search-results-container');
+        let searchShow = fetchFunction(url);
+        searchShow.then(response => {
+            loadingAnimation.style.display = 'none';
+            let results = response.results;
+            if(results.length === 0) {
+                alert('search results not found!');
+            }
+            renderShowsandAnime(results,searchResultsContainer);
+        });
+        input.value = '';
+    });
+}
 
 
 // find page1 or page2:
 let page = document.body.id;
 switch(page){
     case 'homepage':
-        const input = document.querySelector('#input');
-        const searchBtn = document.querySelector('#btn');
-        searchBtn.addEventListener('click',()=>{
-            if(input.value === '' || input.value === null) {
-                window.location.replace('index.html');
-            };
-            const inputValue = input.value;
-            const url = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${inputValue}`;
-            let searchResultsContainer = document.createElement('div');
-            searchResultsContainer.classList.add('search-results-container');
-            let searchShow = fetchFunction(url);
-            searchShow.then(response => {
-                loading.style.display = 'none';
-                let results = response.results;
-                console.log(results.length);
-                if(results.length === 0) {
-                    alert('search results not found!');
-                }
-                displayShows(results,searchResultsContainer);
-            });
-            input.value = '';
-        });
+        searchShowsandAnime();
         getPopularShows();
+        getPopularAnime();
         break;
 
-    case 'showpage':
+    case 'particularshowpage':
         let showId = localStorage.getItem('show_ID');
         let url = `https://api.themoviedb.org/3/tv/${showId}?api_key=${api_key}`;
         let particularShow = fetchFunction(url);
         particularShow.then(response => {
-            loading.style.display = 'none';
-            displayParticularShow(response);
+            loadingAnimation.style.display = 'none';
+            renderParticularShow(response);
         });
         break;
 
